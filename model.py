0f53ca3251e6f4351aab7f4b00cba243c2c1fa29
Jeremy Collins <jeremy.collins@robot-data.ai>
2021-01-02 10:50:31 +0800

    writing new model, including notes

--- model.py
+++ model.py
@@ -5,53 +5,132 @@ import math
 
 
 
+
 class Model():
 
   learn_rate = 0.01
 
-  def __init__(self, number_of_simulations, number_of_samples, number_of_languages):
-
-    '''
-    what is the structure supposed to be?
-    
-    you have the input, which is shape [None, 1, languages]
-    
-    you have a weight for each member of the sample for each member in languages
-    
-    so something like [1, samples, languages]
-    
-    so then multiplying element wise gives
-    
-    [None, samples, languages]
-    
-    you then need to reduce it; or do you?
-    you are simply comparing the prediction for each pair after adding the intercept.
-    
-    so the final output should be of shape
-    
-    [None, samples, languages]
-    
-    in practice the shape of actual is [None, samples, 1] and it can be broadcast I think
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    '''
-
-
+  def __init__(self, number_of_simulations, number_of_samples, number_of_languages, number_of_relatedness_bins, number_of_distance_bins):
     self.sess = tf.Session()
-
-    self.input = tf.placeholder(tf.float32, [None, 1, number_of_languages])
-#     self.input_2 = tf.broadcast_to(self.input, [number_of_simulations, number_of_samples, number_of_languages])
-    
-  
+    self.input = tf.placeholder(tf.float32, [None, 1, number_of_languages])  
     self.output = tf.placeholder(tf.float32, [None, number_of_samples, 1])
-#     self.output_2 = tf.broadcast_to(self.output, [number_of_simulations, number_of_samples, number_of_languages])
+    self.relatedness_array = tf.placeholder(tf.float32, [1, number_of_samples, number_of_languages, number_of_relatedness_bins]
+    self.distance_array = tf.placeholder(tf.float32, [1, number_of_samples, number_of_languages, number_of_distance_bins]
+        
+    self.relatedness_weights = tf.get_variable(name='relatedness_weights', dtype=tf.float32, shape=[1, number_of_relatedness_bins], initializer=tf.truncated_normal_initializer(mean=0.5, stddev=0.01))
+    self.distance_weights = tf.get_variable(name='distance_weights', dtype=tf.float32, shape=[1, number_of_distance_bins], initializer=tf.truncated_normal_initializer(mean=0.5, stddev=0.01))
+    self.intercept = tf.get_variable(name='intercept', dtype = tf.float32, shape = [1],  initializer=tf.truncated_normal_initializer(mean=0.5, stddev=0.01))
+ 
+    
+ 
+    self.r_1 = tf.reshape(self.relatedness_weights, [1, 1, 1, number_of_relatedness_bins)
+    self.d_1 = tf.reshape(self.distance_weights, [1, 1, 1, number_of_distance_bins)
+    
+    self.r_2 = self.relatedness_array * self.r_1
+    self.d_2 = self.distance_array * self.d_1
+    
+    self.r_3 = tf.reduce_sum(self.r_2, axis=3)
+    self.d_3 = tf.reduce_sum(self.d_2, axis=3)
+    
+    self.r_final = self.r_3
+    self.d_final = self.d_3
+    
+    self.relatedness_or_contact_prediction = 1.0 - ((1.0 - self.r_final) * (1.0 - self.d_final) * self.input)
+    self.intercept_prediction = (1.0 - self.r_final) * (1.0 - self.d_final) * self.intercept
+    self.prediction = self.relatedness_or_contact_prediction + self.intercept_prediction
+    
+    '''
+    r_final needs to be of shape [1, number_of_samples, number_of_languages]
+    
+    relatedness weights currently of shape [1, number of relatedness bins]
+    you need to multiply the relatedness array by weights
+    [1, samples, languages, number of relatedness bins]
+    
+    broadcast weights to [1,1,1, relatedness bins]
+    
+    multiply element wise
+    
+    
+    shape [1, samples, languages, number of relatedness bins]
+    
+    then reduce sum along last axis
+    
+    shape [1, samples, languages]
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    ((1 - (1-a)(1-b)) * input)  + ((1-a)(1-b) * intercept)
+    
+    
+    
+    
+    
+    
+    you multiply the input element wise
+    
+    
+    
+   1 - (1-a)(1-b) is the probability of having a shared value between two languages,
+  if you think of a and b as the probability of having a shared value due to relatedness and due to contact respectively
+  
+  then 1 - that * intercept.
+  
+   ????
+   
+   1
+   
+   1
+   
+   0.5
+  
+    0.7
+    
+    
+    
+    you have the probability a of sharing the value due to relatedness; the probability b of sharing the 
+    value due to contact; and the intercept, which is the value assigned if after the two dice rolls neither relatedness
+    nor contact has caused the second language to share the value of the first language
+    
+    so the expected value for language 2 is:
+    
+    sum over the different scenarios:
+    
+    i) shared value due to relatedness: 
+       a * input
+    
+    ii) shared value due to contact:
+      b * input
+      
+    iii) random value
+      (1-a)(1-b) * intercept
+      
+    ^ not sure this is correct
+    
+    easier to simply find the value due to relatedness or contact.
+    
+    i) shared value due to relatedness or contact
+      (1 - (1-a)(1-b)) * input
+    
+    ii) random value
+      (1-a)(1-b) * intercept
+    
+    
+    so the prediction is
+    
+    ((1 - (1-a)(1-b)) * input)  + ((1-a)(1-b) * intercept)
+    
+    '''
+
+
+
+
+
   
     self.weights = tf.get_variable(name='weights', dtype = tf.float32, shape = [1, number_of_samples, number_of_languages],  initializer=tf.truncated_normal_initializer(mean=0.5, stddev=0.01))
     self.intercept = tf.get_variable(name='intercept', dtype = tf.float32, shape = [1],  initializer=tf.truncated_normal_initializer(mean=0.5, stddev=0.01))
@@ -102,77 +181,5 @@ class Model():
     print('Weights: ', weights)
     return weights
 
-class Autoencoder:  
-  learn_rate = 0.01
-  def __init__(self, simulated_feature_array, na_array, number_of_encoding_weights = 400, number_of_features=1, number_of_states=1):
-    self.sess = tf.Session()
-    self.simulated_feature_array = simulated_feature_array
-    self.number_of_languages = np.shape(simulated_feature_array)[1]
-    number_of_languages = self.number_of_languages
-    number_of_simulations = simulated_feature_array.shape[0] 
-    self.na_array = na_array
-    self.encoder_input = tf.placeholder(tf.float32, [None, number_of_languages])
-    self.encoder_weights = tf.get_variable(name='encoder_weights', dtype = tf.float32, shape = [number_of_languages, number_of_encoding_weights],  initializer=tf.truncated_normal_initializer(mean=0.5, stddev=0.01))
-    self.encoder_weights_sum = tf.reduce_sum(self.encoder_weights, axis = 0)
-    self.encoder_weights_sum = tf.broadcast_to(self.encoder_weights_sum, [number_of_languages, number_of_encoding_weights])
-    self.encoder_weights_normalised = self.encoder_weights / self.encoder_weights_sum
-    self.encoding = tf.matmul(self.encoder_input, self.encoder_weights_normalised)
 
-    
-    self.intercept = tf.get_variable(name='intercept', dtype=tf.float32, shape=[1], initializer=tf.truncated_normal_initializer(mean=0.5, stddev=0.01))
 
-    ''' going to comment these out'''
-#     self.intercept_weights = tf.get_variable(name='intercept_weights', dtype=tf.float32, shape=[1,number_of_encoding_weights], initializer=tf.truncated_normal_initializer(mean=0.5, stddev=0.01))
-#     self.encoding_after_weights = self.encoding * (1.0 - self.intercept_weights)
-#     self.intercept_after_weights = self.intercept_weights * self.intercept
-#     self.encoding = self.encoding_after_weights + self.intercept_after_weights    
-   
-    self.decoder_weights = tf.get_variable(name='decoder_weights', dtype = tf.float32, shape = [number_of_encoding_weights, number_of_languages],  initializer=tf.truncated_normal_initializer(mean=0.5, stddev=0.01))   
-    self.decoder_weights_sum = tf.reduce_sum(self.decoder_weights, axis = 0)
-    self.decoder_weights_sum = tf.broadcast_to(self.decoder_weights_sum, [number_of_encoding_weights, number_of_languages])
-    self.decoder_weights_normalised = self.decoder_weights / self.decoder_weights_sum
-    
-    self.prediction = tf.matmul(self.encoding, self.decoder_weights_normalised)
-    
-    '''
-    want a final intercept
-    '''
-    
-    self.final_intercept_weighting = tf.get_variable(name='final_intercept', dtype=tf.float32, shape=[1], initializer=tf.truncated_normal_initializer(mean=0.5, stddev=0.01))
-    self.prediction = (self.prediction * (1 - self.final_intercept_weighting)) + (self.intercept * self.final_intercept_weighting)
-    
-    self.actual = self.encoder_input
-    self.loss = tf.log(1.0 - tf.abs(self.actual - self.prediction))
-    self.total_loss = tf.reduce_mean(self.loss) * -1.0
-    
-  def train(self, steps=10000):
-    self.train_step = tf.train.AdamOptimizer(self.learn_rate).minimize(self.total_loss)  
-    self.encoder_clip = tf.assign(self.encoder_weights, tf.clip_by_value(self.encoder_weights, 0, 1))
-    self.decoder_clip = tf.assign(self.decoder_weights, tf.clip_by_value(self.decoder_weights, 0, 1))
-    self.intercept_clip = tf.assign(self.intercept, tf.clip_by_value(self.intercept, 0, 1))
-#     self.intercept_weights_clip = tf.assign(self.intercept_weights, tf.clip_by_value(self.intercept_weights, 0, 1))
-    self.final_intercept_weighting_clip = tf.assign(self.final_intercept_weighting, tf.clip_by_value(self.final_intercept_weighting, 0, 1))
-    init = tf.initialize_all_variables()
-    self.sess.run(init)   
-    self.feed = {self.encoder_input: self.simulated_feature_array}
-    for i in range(steps):  
-      self.sess.run(self.train_step, feed_dict = self.feed)
-      self.sess.run(self.encoder_clip)
-      self.sess.run(self.decoder_clip)
-      self.sess.run(self.intercept_clip)
-#       self.sess.run(self.intercept_weights_clip)
-      self.sess.run(self.final_intercept_weighting_clip)
-      print("After %d iterations:" % i)
-      print(self.sess.run(self.prediction, feed_dict=self.feed))
-      print(self.sess.run(self.actual, feed_dict=self.feed))
-      print(self.sess.run(self.total_loss, feed_dict=self.feed))
-#       print(self.sess.run(self.intercept))
-#       print(self.sess.run(self.final_intercept_weighting))
-#       print(self.sess.run(tf.reduce_min(self.encoding), feed_dict=self.feed))
-#       print(self.sess.run(self.encoder_weights, feed_dict=self.feed))
-
-  def show_loss(self, test_data):
-    feed = {self.encoder_input: test_data}
-    loss = self.sess.run(self.total_loss, feed_dict= feed)
-    print('Loss: ', loss)
-    return loss
